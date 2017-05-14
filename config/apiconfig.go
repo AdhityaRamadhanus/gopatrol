48dccb7b2f354e914d5221dfb69e8f577c59cc87
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2017-05-14 17:13:24 +0700

    remove unnecessary config

--- config/apiconfig.go
+++ config/apiconfig.go
@@ -6,14 +6,12 @@ var (
 
 // Config provides the configuration for the API server
 type Config struct {
-	Logging    bool
 	EnableCors bool
 	Address    string
 }
 
 func GetDefaultConfig() *Config {
 	return &Config{
-		Logging:    true,
 		EnableCors: true,
 		Address:    ":3000",
 	}
--- config/checkup.go
+++
@@ -1,5 +0,0 @@
-package config
-
-type CheckupConfig struct {
-	ConnectionString string `json:"connection_string"` // MongoDB connection string
-}
